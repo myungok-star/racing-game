40e8e24344e8b870487e3aa8f3a3074833838360
myungok-star <myungok.star@gmail.com>
2016-11-06 14:17:06 -0800

    added a keydown  function to move players

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
 
-//global variables
 
-var winner = null;
-
-
+  $(document).keydown(function(key) {
+    switch(parseInt(key.which,10)) {
+      //player-top: when "a" is clicked, move to the right
+       case 65:
+				$('#player-top').animate({left: "+=25px"}, 'fast');
+        break;
+      //player-bottom: when "l" is clicked, move to the right
+      case 76:
+        $('#player-bottom').animate({left: "+=25px"}, 'fast');
+        break;
+}
+  });
 
 
 
