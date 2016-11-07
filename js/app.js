edb8080b4d3b28948ad95c97fe9a4068ceb31085
myungok-star <myungok.star@gmail.com>
2016-11-06 22:32:46 -0800

    updated movePlayer function to avoid DRY

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
-
-  $(document).keydown(function(key) {
+//first attempt
+/*  $(document).keydown(function(key) {
     switch(parseInt(key.which,10)) {
       //player-top: when "a" is clicked, move to the right
-       case 65:
+      case 65:
 				$('#player-top').animate({left: "+=25px"}, 'fast');
         break;
       //player-bottom: when "l" is clicked, move to the right
@@ -12,10 +12,26 @@ $(document).ready(function () {
         $('#player-bottom').animate({left: "+=25px"}, 'fast');
         break;
 }
+  });*/
+
+//second version
+  function movePlayer($player) {
+    $player.animate({ left: '+=25px' }, 'fast');
+    // check new position
+  }
+
+  $(document).keydown(function(key) {
+    switch(parseInt(key.which,10)) {
+      //player-top: when "a" is clicked, move to the right
+      case 65:
+        movePlayer($('#player-top'));
+        break;
+      //player-bottom: when "l" is clicked, move to the right
+      case 76:
+        movePlayer($('#player-bottom'));
+        break;
+    }
   });
-
-
-
 
 
 
