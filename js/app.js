8341fbaa24a0e7a81a9652e826c61975f215b063
WDI-Box <WDI-Box@WDI-Boxs-MacBook-Pro.local>
2017-01-02 16:53:02 -0600

    added suggestions

--- js/app.js
+++ js/app.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
 
     var winner = null;
     //executeWinner function
+    //TODO: move this to outside your document ready call
     function executeWinner() {
         alert("You are the WINNER!!!")
         $('#reset').removeClass('tmp-hidden');
     }
 
     //play again function
+    //TODO: move this to outside your document ready call. you'll probably want to set winner as a global. use global convention WINNER
     function playAgain() {
         $('#reset').addClass('tmp-hidden');
         $('.player').css({
@@ -16,6 +18,7 @@ $(document).ready(function() {
         winner = null;
 
     }
+    // TODO: remove unused code.
     //first attempt of moving players
     /*  $(document).keydown(function(key) {
         switch(parseInt(key.which,10)) {
@@ -31,6 +34,7 @@ $(document).ready(function() {
       });*/
 
     //second version
+    // TODO: remove from document ready
     function movePlayer($player) {
         $player.animate({
             left: '+=30px'
@@ -39,13 +43,14 @@ $(document).ready(function() {
         //find the winner
         var currentPosition = $player.offset().left;
         /*$player.css({left: currentPosition + 30});*/
+        // TODO: extract this conditional to a variable, makes it easier to understand
         if ($player.offset().left >= $('.game-board').width() - $player.width()) {
-
             // set winner to player and execute win
             winner = $player;
             executeWinner();
         }
     }
+
     $(document).keydown(function(key) {
         switch (parseInt(key.which, 10)) {
             //player-top: when "a" is clicked, move to the right
@@ -60,6 +65,8 @@ $(document).ready(function() {
         }
     });
 
+    // TODO: adhere to a consistent convention for defining functions
+    // TODO: move this to outside of document ready. Also, when you call race and have the above document keydown listener and window keypress listener executing, which runs first? Which should run first? Do they need to be separate?
     var race = function() {
         $(window).on('keypress', function(event) {
             if (!winner) {
